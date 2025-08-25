0ac4126706e598e1f14c0701547dcf1fc12d9f6f
Kevin Bogoreli <bogoreli.kevin@outlook.com>
2025-08-25 14:31:09 -0300

    chore: Add setup Drizzle ORM

---
+++ src/db/schema.ts
@@ -0,0 +1,53 @@
+import { relations } from "drizzle-orm";
+import { uuid } from "drizzle-orm/gel-core";
+import {
+  date,
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  email: varchar("email", { length: 150 }).notNull(),
+  password: text("password").notNull(),
+});
+
+export const transactionType = pgEnum("transaction_type", [
+  "income",
+  "expense",
+]);
+
+export const categoryTable = pgTable("categories", {
+  id: uuid("id").primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+});
+
+export const transactionsTable = pgTable("transactions", {
+  id: uuid("id").primaryKey(),
+  type: transactionType("type").notNull(),
+  categoryId: uuid("category_id")
+    .notNull()
+    .references(() => categoryTable.id),
+  description: text("description"),
+  amount: integer().notNull(),
+  crearedAt: date("created_at").notNull().defaultNow(),
+});
+
+export const categoryRelations = relations(categoryTable, (params) => {
+  return {
+    categories: params.many(transactionsTable),
+  };
+});
+
+export const transactionRelations = relations(transactionsTable, (params) => {
+  return {
+    category: params.one(categoryTable, {
+      fields: [transactionsTable.categoryId],
+      references: [categoryTable.id],
+    }),
+  };
+});
